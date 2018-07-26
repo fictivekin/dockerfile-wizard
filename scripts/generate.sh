960fca8fb316a73959ee4e872f48111e40f05ea0
Jared McKnight <jmcknight@archertech.ca>
2018-07-26 09:30:03 -0400

    setup utf-8 locale inside container

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -136,3 +136,10 @@ RUN apt-get -y install libgconf-2-4 \
   && mv chromedriver /usr/local/bin/chromedriver \
   && chmod +x /usr/local/bin/chromedriver"
 fi
+
+# setup utf-8
+echo "# Set the locale
+RUN locale-gen en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8"
